d453674f6fd2501b1d452fb3973512117a25e88b
mnemotechnician <daytimer253@gmail.com>
2022-02-02 14:39:25 +0300

    yeah, i do be forgorring 💀

--- app/src/main/java/com/ncorti/kotlin/template/app/MainActivity.kt
+++ app/src/main/java/com/ncorti/kotlin/template/app/MainActivity.kt
@@ -19,19 +19,13 @@ class MainActivity : AppCompatActivity() {
 			if (binding.editTextFactorial.text.isNotEmpty()) {
 				val input = binding.editTextFactorial.text.toString().toInt()
 				val result = input.let {
-					var i = 1
-					for (i in 1..it) i *= i
-					i
-				}
+					var result = 1
+					for (i in 1..it) result *= i
+					result
+				}.toString()
 
 				binding.textResult.text = result
 				binding.textResult.visibility = View.VISIBLE
-
-				notificationUtil.showNotification(
-					context = this,
-					title = getString(R.string.notification_title),
-					message = result
-				)
 			} else {
 				Toast.makeText(this, "Please enter a number", Toast.LENGTH_SHORT).show()
 			}
